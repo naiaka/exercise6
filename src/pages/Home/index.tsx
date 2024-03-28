0e84906cee4ffd68c7094232f160a9e6e10b9483
Kanaya <kanayasectio@gmail.com>
2024-03-28 23:51:22 +0800

    MoneyTracker

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,15 +1,101 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {ScrollView, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
-import {PageHeader} from '../../components';
+import {PageHeader, Gap, Button} from '../../components';
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
-    <View>
+    <ScrollView style={styles.container}>
       <PageHeader type="withPhoto" />
-    </View>
+      <Gap height={24} />
+      <View style={styles.contentWrapper}>
+        <Text style={styles.balanceText}>Your balance</Text>
+        <Text style={styles.balanceAmount}>Rp. 10.000.000</Text>
+        <View style={styles.line} />
+        <View style={styles.subBalanceWrapper}>
+          <Text style={styles.subBalanceTextLeft}>Cash on Hand</Text>
+          <Text style={styles.subBalanceTextRight}>Rp. 4.000.000</Text>
+        </View>
+        <View style={styles.subBalanceWrapper}>
+          <Text style={styles.subBalanceTextLeft}>Cash on Bank</Text>
+          <Text style={styles.subBalanceTextRight}>Rp. 6.000.000</Text>
+          <Gap height={50} />
+        </View>
+      </View>
+      <Gap height={24} />
+      <View style={styles.contentWrapper}>
+        <Gap height={24} />
+        <Text style={styles.transaction}> Add Transaction</Text>
+        <Button
+          label="Cash on Hand"
+          backgroundColor="#02CF8E"
+          textColor="#020202"
+          onPress={() => navigation.navigate('CashonHand')}
+        />
+        <Gap height={20} />
+        <Button
+          label="Cash on Bank"
+          backgroundColor="#02CF8E"
+          textColor="#020202"
+          onPress={() => navigation.navigate('CashonBank')}
+        />
+        <Gap height={30} />
+      </View>
+    </ScrollView>
   );
 };
 
 export default Home;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentWrapper: {
+    backgroundColor: '#FFFFFF',
+    flex: 1,
+  },
+  balanceText: {
+    fontSize: 16,
+    fontFamily: 'Poppins-Light',
+    color: '#000000',
+    marginTop: 20,
+    paddingLeft: 24,
+  },
+  balanceAmount: {
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 10,
+    fontFamily: 'Poppins-Regular',
+    color: '#000000',
+  },
+  line: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#000000',
+    marginHorizontal: 16,
+    marginTop: 10,
+  },
+  subBalanceWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    marginTop: 10,
+  },
+  subBalanceTextLeft: {
+    flex: 1,
+    textAlign: 'left',
+    fontFamily: 'Poppins-Regular',
+    color: '#000000',
+  },
+  subBalanceTextRight: {
+    flex: 1,
+    fontFamily: 'Poppins-Regular',
+    color: '#000000',
+  },
+  transaction: {
+    fontSize: 16,
+    color: '#000000',
+    fontFamily: 'Poppins-Regular',
+    paddingBottom: 15,
+    marginLeft: 24,
+  },
+});
