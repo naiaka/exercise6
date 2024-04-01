12b06601ee6a62c49f80bca31ac15f0e7c7bef02
Kanaya <kanayasectio@gmail.com>
2024-04-02 02:41:42 +0800

    MoneyTacker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,5 +2,7 @@ import SplashScreen from './SplashScreen';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import Home from './Home';
+import CashOnHand from './CashOnHand';
+import CashOnBank from './CashOnBank';
 
-export {SplashScreen, SignIn, SignUp, Home};
+export {SplashScreen, SignIn, SignUp, Home, CashOnHand, CashOnBank};
